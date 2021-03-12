ad4559035b0be8d819a4087db2b11ea5d197032e
leandro simonato <leandrosimonato@pop-os.localdomain>
2021-03-12 20:33:07 -0300

    Manipule o DOM no JavaScript

---
+++ componetes/deletaTarefa.js
@@ -0,0 +1,22 @@
+
+const BotaoDeleta = () => {
+    const botaoDeleta = document.createElement('button')
+
+    botaoDeleta.innerText = 'deletar'
+    botaoDeleta.addEventListener('click', deletarTarefa)
+
+    return botaoDeleta
+}
+
+const deletarTarefa = (evento) => {
+    const botaoDeleta = evento.target
+
+    const tarefaCompleta = botaoDeleta.parentElement
+
+    tarefaCompleta.remove()
+
+    return botaodeleta
+
+}
+
+export default BotaoDeleta
